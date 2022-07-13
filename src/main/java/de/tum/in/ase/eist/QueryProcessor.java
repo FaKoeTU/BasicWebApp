0a16f5ab1e2267f704c7cff854a1a7c97689433e
FK0911 <fabian.koenig1@alumni.fh-aachen.de>
2022-07-13 13:06:47 +0200

    123

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -13,8 +13,23 @@ public class QueryProcessor {
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
            return "Fabian";
-        } else { // TODO extend the programm here
-            return "";
+        } else if (query.contains("largest")){ // TODO extend the programm here
+            String[] result = query.split(" ");
+            int biggest = 0;
+            //%20which%20of%20the%20following%20numbers%20is%20the%20largest:%2021,%2097,%201,%20289"
+            for(int i = 0; i < result.length; i++){
+                if(result[i] == "largest:"){
+
+                    for(int j = i; j < result.length; j++) {
+                        int value = Integer.parseInt(result[i].split(",")[0]);
+                        if(value>biggest) {
+                            biggest = value;
+                        }
+                    }
+                    break;
+                }
+            }
+            return Integer.toString(biggest);
         }
     }
 }
